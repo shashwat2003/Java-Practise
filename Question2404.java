ac26ffd39d8de54462e58c658da738475d2017cc
shashwat2003 <info.superpcstduio@gmail.com>
2022-04-24 12:26:56 +0530

    Updated Question 24/04

--- Question2404.java
+++ Question2404.java
@@ -8,11 +8,15 @@ public class Question2404 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        if (n < 3) {
+            System.out.println(false);
+        } else {
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(isMountain(arr));
         }
-        System.out.println(isMountain(arr));
+
         sc.close();
     }
 
